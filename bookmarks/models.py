7f03a295c181f0adc4380173656478fba045336c
gooseandmegander <meganawilliamson@gmail.com>
2018-06-25 14:49:43 -0700

    made changes to Note and BookMark models

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 from uuid import uuid4
 
+class BookMark_Folder(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    folder_name = models.CharField(max_length=50, unique=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
 class BookMark(models.Model):
-    id = models.UUIDField(primary_key = True, default = uuid4, editable = False)
-    bookmark_name = models.CharField(max_length = 200)
-    bookmark_folder = models.CharField(max_length = 200)
-    hyperlink = models.URLField(max_length = 200)
-    created_at = models.DateTimeField(auto_now_add = True)
-    last_modified = models.DateTimeField(auto_now = True)
-
-
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    bookmark_name = models.CharField(max_length=200)
+    bookmark_folder = models.ForeignKey(BookMark_Folder, null=True, unique=True, blank=True, on_delete=models.SET_NULL)
+    hyperlink = models.URLField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
--- notes/models.py
+++ notes/models.py
@@ -1,13 +1,25 @@
 from django.db import models
 from uuid import uuid4
 
+class Tag(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    tag_name = models.CharField(max_length=50, unique=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    category_name = models.CharField(max_length=50, unique=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
 class Note(models.Model):
     # TODO: Add user/author who created it
     id = models.UUIDField(primary_key = True, default = uuid4, editable = False)
     title = models.CharField(max_length = 200)
     content = models.TextField(blank = True)
-    tags = models.TextField(blank = True, default='all')
-    categories = models.CharField(max_length = 200, default='all')
+    tags = models.ForeignKey(Tag, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add = True)
     last_modified = models.DateTimeField(auto_now = True)
 
